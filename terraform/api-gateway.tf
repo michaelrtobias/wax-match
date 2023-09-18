a5caf9347450cd1413fd98d3b1555c3d5b9f3988
Michael Tobias <michaelremitobias@gmail.com>
2023-09-18 10:53:44 -0700

    in between

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -72,12 +72,62 @@ resource "aws_api_gateway_resource" "discogs_auth" {
   path_part   = "auth"
 
 }
+
+// discogs auth options
+
+// discogs auth get
 resource "aws_api_gateway_resource" "discogs_auth_request_token" {
   rest_api_id = aws_api_gateway_rest_api.waxmatch.id
   parent_id   = aws_api_gateway_resource.discogs_auth.id
   path_part   = "request-token"
 }
 
+resource "aws_api_gateway_method" "discogs_auth_request_token_options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.waxmatch.id
+  resource_id   = aws_api_gateway_resource.discogs_auth_request_token.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_method_response" "discogs_auth_request_token_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.waxmatch.id
+  resource_id = aws_api_gateway_resource.discogs_auth_request_token.id
+  http_method = aws_api_gateway_method.discogs_auth_request_token_options_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = true,
+    "method.response.header.Access-Control-Allow-Methods"     = true,
+    "method.response.header.Access-Control-Allow-Origin"      = true,
+    "method.response.header.Access-Control-Allow-Credentials" = true
+  }
+  depends_on = [aws_api_gateway_method.discogs_auth_request_token_options_method]
+}
+resource "aws_api_gateway_integration" "discogs_auth_request_token_options_integration" {
+  rest_api_id          = aws_api_gateway_rest_api.waxmatch.id
+  resource_id          = aws_api_gateway_resource.discogs_auth_request_token.id
+  http_method          = aws_api_gateway_method.discogs_auth_request_token_options_method.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+  request_templates = {
+    "application/json" = "{ 'statusCode': 200 }"
+  }
+  depends_on = [aws_api_gateway_method.discogs_auth_request_token_options_method]
+}
+resource "aws_api_gateway_integration_response" "discogs_auth_request_token_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.waxmatch.id
+  resource_id = aws_api_gateway_resource.discogs_auth_request_token.id
+  http_method = aws_api_gateway_method.discogs_auth_request_token_options_method.http_method
+  status_code = aws_api_gateway_method_response.discogs_auth_request_token_options_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods"     = "'GET,OPTIONS'",
+    "method.response.header.Access-Control-Allow-Origin"      = "'*'",
+    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+  }
+  depends_on = [aws_api_gateway_method_response.discogs_auth_request_token_options_200]
+}
 resource "aws_api_gateway_method" "discogs_auth_request_token" {
   rest_api_id   = aws_api_gateway_rest_api.waxmatch.id
   resource_id   = aws_api_gateway_resource.discogs_auth_request_token.id
@@ -87,14 +137,14 @@ resource "aws_api_gateway_method" "discogs_auth_request_token" {
 }
 
 resource "aws_api_gateway_method_response" "discogs_auth_request_token_200" {
-  rest_api_id         = aws_api_gateway_rest_api.waxmatch.id
-  resource_id         = aws_api_gateway_resource.discogs_auth_request_token.id
-  http_method         = aws_api_gateway_method.discogs_auth_request_token.http_method
-  response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
-  response_models = {
-    "application/json" = "Empty"
-  }
+  rest_api_id = aws_api_gateway_rest_api.waxmatch.id
+  resource_id = aws_api_gateway_resource.discogs_auth_request_token.id
+  http_method = aws_api_gateway_method.discogs_auth_request_token.http_method
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  depends_on = [aws_api_gateway_method.discogs_auth_request_token]
 }
 resource "aws_api_gateway_method_response" "discogs_auth_request_token_400" {
   rest_api_id = aws_api_gateway_rest_api.waxmatch.id
@@ -110,6 +160,7 @@ resource "aws_api_gateway_integration" "discogs_auth_request_token" {
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = local.discogs_lambdas["discogs-request-token"].invoke_arn
+  depends_on              = [aws_api_gateway_method.discogs_auth_request_token]
 }
 
 resource "aws_api_gateway_integration_response" "discogs_auth_request_token" {
