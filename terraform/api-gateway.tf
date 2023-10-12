8a2acece9875a60ecd5950ec0190b8959050387c
Michael Tobias <michaelremitobias@gmail.com>
2023-10-11 17:37:33 -0700

    all 4 routes have an options request

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -414,8 +414,57 @@ resource "aws_api_gateway_resource" "discogs_collection_releases" {
   path_part   = "releases"
 }
 
+// discogs releases options
+
+resource "aws_api_gateway_method" "discogs_collection_releases_options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.waxmatch.id
+  resource_id   = aws_api_gateway_resource.discogs_collection_releases.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_method_response" "discogs_collection_releases_options_200" {
+  rest_api_id = aws_api_gateway_rest_api.waxmatch.id
+  resource_id = aws_api_gateway_resource.discogs_collection_releases.id
+  http_method = aws_api_gateway_method.discogs_collection_releases_options_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = true,
+    "method.response.header.Access-Control-Allow-Methods"     = true,
+    "method.response.header.Access-Control-Allow-Origin"      = true,
+    "method.response.header.Access-Control-Allow-Credentials" = true
+  }
+  depends_on = [aws_api_gateway_method.discogs_collection_releases_options_method]
+}
+resource "aws_api_gateway_integration" "discogs_collection_releases_options_integration" {
+  rest_api_id          = aws_api_gateway_rest_api.waxmatch.id
+  resource_id          = aws_api_gateway_resource.discogs_collection_releases.id
+  http_method          = aws_api_gateway_method.discogs_collection_releases_options_method.http_method
+  type                 = "MOCK"
+  passthrough_behavior = "WHEN_NO_MATCH"
+  request_templates = {
+    "application/json" = "{ 'statusCode': 200 }"
+  }
+  depends_on = [aws_api_gateway_method.discogs_collection_releases_options_method]
+}
+resource "aws_api_gateway_integration_response" "discogs_collection_releases_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.waxmatch.id
+  resource_id = aws_api_gateway_resource.discogs_collection_releases.id
+  http_method = aws_api_gateway_method.discogs_collection_releases_options_method.http_method
+  status_code = aws_api_gateway_method_response.discogs_collection_releases_options_200.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods"     = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"      = "'*'",
+    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
+  }
+  depends_on = [aws_api_gateway_method_response.discogs_collection_releases_options_200]
+}
+
 // discogs get collection releases
-resource "aws_api_gateway_method" "discogs_collection_releases" {
+resource "aws_api_gateway_method" "discogs_collection_releases_get" {
   rest_api_id   = aws_api_gateway_rest_api.waxmatch.id
   resource_id   = aws_api_gateway_resource.discogs_collection_releases.id
   http_method   = "GET"
@@ -423,10 +472,10 @@ resource "aws_api_gateway_method" "discogs_collection_releases" {
   authorizer_id = aws_api_gateway_authorizer.waxmatch.id
 }
 
-resource "aws_api_gateway_method_response" "discogs_collection_releases_200" {
+resource "aws_api_gateway_method_response" "discogs_collection_releases_get_200" {
   rest_api_id         = aws_api_gateway_rest_api.waxmatch.id
   resource_id         = aws_api_gateway_resource.discogs_collection_releases.id
-  http_method         = aws_api_gateway_method.discogs_collection_releases.http_method
+  http_method         = aws_api_gateway_method.discogs_collection_releases_get.http_method
   response_parameters = { "method.response.header.Access-Control-Allow-Origin" = true }
   response_models = {
     "application/json" = "Empty"
@@ -434,26 +483,26 @@ resource "aws_api_gateway_method_response" "discogs_collection_releases_200" {
   status_code = "200"
 }
 
-resource "aws_api_gateway_method_response" "discogs_collection_releases_400" {
+resource "aws_api_gateway_method_response" "discogs_collection_releases_get_400" {
   rest_api_id = aws_api_gateway_rest_api.waxmatch.id
   resource_id = aws_api_gateway_resource.discogs_collection_releases.id
-  http_method = aws_api_gateway_method.discogs_collection_releases.http_method
+  http_method = aws_api_gateway_method.discogs_collection_releases_get.http_method
   status_code = "400"
 }
 
-resource "aws_api_gateway_integration" "discogs_collection_releases" {
+resource "aws_api_gateway_integration" "discogs_collection_releases_get" {
   rest_api_id             = aws_api_gateway_rest_api.waxmatch.id
   resource_id             = aws_api_gateway_resource.discogs_collection_releases.id
-  http_method             = aws_api_gateway_method.discogs_collection_releases.http_method
-  integration_http_method = "GET"
+  http_method             = aws_api_gateway_method.discogs_collection_releases_get.http_method
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = local.discogs_lambdas["discogs-get-collection-releases"].invoke_arn
 }
 
-resource "aws_api_gateway_integration_response" "discogs_collection_releases" {
+resource "aws_api_gateway_integration_response" "discogs_collection_releases_get" {
   rest_api_id = aws_api_gateway_rest_api.waxmatch.id
   resource_id = aws_api_gateway_resource.discogs_collection_releases.id
-  http_method = aws_api_gateway_method.discogs_collection_releases.http_method
+  http_method = aws_api_gateway_method.discogs_collection_releases_get.http_method
   status_code = "200"
 
   response_templates = {
